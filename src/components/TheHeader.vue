748232a605d6a24bcfa22176b141c7672d3fbca8
Xaviel Cesar Joao <xavielcesar.joao@gmail.com>
2024-01-18 12:02:26 +0100

    create new componente for teste

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+        Cabeçalho principal da pagina 2
+    </header>
+</template>
+
+<script>
+    export default {
+        
+
+    }
+
+</script>
+
+<style>
+
+    .header {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 30px;
+        color: brown;
+
+    }
+</style>
